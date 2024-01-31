fb62181cc9fe113bdc6fc3980dfe122457dc9d56
Priya Tyagi <priyatyagi1905@gmail.com>
2024-01-31 15:46:06 -0800

    completed attraction page also added form.js

---
+++ scripts/form.js
@@ -0,0 +1,9 @@
+// took this code from W3schools
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
